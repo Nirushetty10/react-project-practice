5ce6d32a5dd3e95b2a23c8518498e0e3339577d8
Nirushetty10 <nirushetty582@gmail.com>
2023-02-28 10:07:29 +0530

    new ref

---
+++ Refs/Playpause.jsx
@@ -0,0 +1,26 @@
+import React,{useState, useRef} from 'react'
+import Video from "./videos/videoplayback.mp4"
+
+function Playpause() {
+    let [video, setVideo] = useState(Video)
+    let [play,setPlay] = useState(false)
+    let videoRef = useRef()
+
+    let handleChange = () =>{
+        if(!play) {
+            setPlay(true)
+            videoRef.current.play()
+        } else {
+            setPlay(false)
+            videoRef.current.pause()
+        }
+    }
+
+  return (
+    <div>
+        <video src={video} ref={videoRef} onClick={handleChange}></video>
+    </div>
+  )
+}
+
+export default Playpause
